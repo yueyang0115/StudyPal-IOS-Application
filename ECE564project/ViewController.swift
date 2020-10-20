a1ac90bcac26f6bdb8dae0847bd50a0ddafc908d
yueyang <yueyang0115@outlook.com>
2020-10-20 01:18:31 -0400

    add drawing functions

--- ECE564project/ViewController.swift
+++ ECE564project/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import PencilKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PKCanvasViewDelegate, PKToolPickerObserver {
 
+    @IBOutlet weak var pencilButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var canvasView: PKCanvasView!
+    
+    let canvasWidth: CGFloat = 768
+    let canvasHeight:CGFloat = 500
+    var drawing = PKDrawing()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setCanvas()
+    }
+    
+    func setCanvas(){
+        canvasView.delegate = self
+        canvasView.drawing = drawing
+        canvasView.alwaysBounceVertical = true
+        canvasView.allowsFingerDrawing = true
+        
+        if let window = parent?.view.window,
+        let toolPicker = PKToolPicker.shared(for: window){
+            toolPicker.setVisible(true, forFirstResponder: canvasView)
+            toolPicker.addObserver(canvasView)
+            canvasView.becomeFirstResponder()
+        }
     }
 
-
+    @IBAction func saveImageToAlbum(_ sender: Any) {
+    }
+    
+    @IBAction func changePencilFinger(_ sender: Any){
+        canvasView.allowsFingerDrawing.toggle()
+        pencilButton.title = canvasView.allowsFingerDrawing ? "Finger" : "Pencil"
+    }
+    
+    // - MARK: make rotate view work
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let canvasScale = canvasView.bounds.width / canvasWidth
+        canvasView.minimumZoomScale = canvasScale
+        canvasView.maximumZoomScale = canvasScale
+        canvasView.zoomScale = canvasScale
+        updateContentSize()
+        canvasView.contentOffset = CGPoint(x: 0, y: -canvasView.adjustedContentInset.top)
+    }
+    
+    override var prefersHomeIndicatorAutoHidden: Bool{
+        return true
+    }
+    
+    func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+        updateContentSize()
+    }
+    
+    func updateContentSize(){
+        let drawing = canvasView.drawing
+        let contentHeight: CGFloat
+        if !drawing.bounds.isNull{
+            contentHeight = max(canvasView.bounds.height, (drawing.bounds.maxY + self.canvasHeight) * canvasView.zoomScale)
+        }
+        else{
+            contentHeight = canvasView.bounds.height
+        }
+        canvasView.contentSize = CGSize(width: canvasWidth * canvasView.zoomScale, height: contentHeight)
+    }
 }
 
