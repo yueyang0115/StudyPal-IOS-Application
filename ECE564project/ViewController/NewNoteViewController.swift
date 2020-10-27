850b49102e4b1b1daceeb623f6b8cccdb4431891
yueyang <yueyang0115@outlook.com>
2020-10-27 12:17:06 -0400

    add view controller for new note view

---
+++ ECE564project/ViewController/NewNoteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewNoteViewController.swift
+//  ECE564project
+//
+//  Created by 杨越 on 10/27/20.
+//  Copyright © 2020 杨越. All rights reserved.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
