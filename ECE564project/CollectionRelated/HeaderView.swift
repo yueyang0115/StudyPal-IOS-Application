9dfc64629414f1ae1272d0d93e5e89499e67eed5
yueyang <yueyang0115@outlook.com>
2020-11-03 02:13:58 -0500

    change to collection view

---
+++ ECE564project/CollectionRelated/HeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderView.swift
+//  ECE564project
+//
+//  Created by 杨越 on 11/3/20.
+//  Copyright © 2020 杨越. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UICollectionReusableView{
+    
+    @IBOutlet weak var catogoryImage: UIImageView!
+    @IBOutlet weak var catogoryLabel: UILabel!
+    
+}
+
---
+++ ECE564project/CollectionRelated/NoteCell.swift
@@ -0,0 +1,15 @@
+//
+//  NoteCell.swift
+//  ECE564project
+//
+//  Created by 杨越 on 11/3/20.
+//  Copyright © 2020 杨越. All rights reserved.
+//
+
+import UIKit
+
+class NoteCell: UICollectionViewCell {
+    
+    @IBOutlet weak var noteImage: UIImageView!
+    
+}
