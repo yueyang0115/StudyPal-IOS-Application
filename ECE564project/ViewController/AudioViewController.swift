d616f5d5ccd457e0d1e97d08b8b7779f12ef2641
yueyang <yueyang0115@outlook.com>
2020-11-08 17:53:55 -0500

    add message for playing and recording

--- ECE564project/ViewController/AudioViewController.swift
+++ ECE564project/ViewController/AudioViewController.swift
@@ -16,22 +16,27 @@ class AudioViewController: UIViewController, AVAudioRecorderDelegate, UITableVie
     var audioPlayer: AVAudioPlayer!
     var numberOfRecording: Int = 0
     
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet var StartStopButton: UIBarButtonItem!
     @IBOutlet weak var recordingTableView: UITableView!
-    @IBOutlet weak var startButton: UIButton!
+
+    var buttonIdx = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        setUpRecorder()
+        setUpLabel()
+    }
+    
+    func setUpRecorder(){
         // set up session
         recordingSession = AVAudioSession.sharedInstance()
-        
         // get previous numOfRecording
         if let number:Int = UserDefaults.standard.object(forKey: "recodingNumber") as? Int{
             numberOfRecording = number
         }
-        
-        
         AVAudioSession.sharedInstance().requestRecordPermission{ (hasPermission) in
             if hasPermission{
                 print("ACCEPTED")
@@ -39,8 +44,17 @@ class AudioViewController: UIViewController, AVAudioRecorderDelegate, UITableVie
         }
     }
     
-    @IBAction func record(_ sender: Any) {
-        if(audioRecorder == nil){
+    func setUpLabel(){
+        message.lineBreakMode = NSLineBreakMode.byWordWrapping
+        message.numberOfLines = 2
+    }
+    
+    // MARK: - record, play and stop
+    
+    @objc @IBAction func recordORstop(_ sender: Any) {
+        if(audioRecorder == nil && audioPlayer == nil){
+            // start recoding
+            changeButtonToStop()
             numberOfRecording += 1
             let filename = getDirectory().appendingPathComponent("\(numberOfRecording).m4a")
             let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
@@ -50,7 +64,7 @@ class AudioViewController: UIViewController, AVAudioRecorderDelegate, UITableVie
                 audioRecorder = try AVAudioRecorder(url: filename, settings: settings)
                 audioRecorder.delegate = self
                 audioRecorder.record()
-                startButton.setTitle("stop Recording", for: .normal)
+                message.text = "Audio is recording..."
             }
             catch{
                 displayAlert(title: "Ups!", message: "Recording failed!")
@@ -58,12 +72,19 @@ class AudioViewController: UIViewController, AVAudioRecorderDelegate, UITableVie
         }
         else{
             // stop recoding
-            audioRecorder.stop()
-            audioRecorder = nil
-            
-            UserDefaults.standard.set(numberOfRecording, forKey: "recodingNumber")
-            recordingTableView.reloadData()
-            startButton.setTitle("start Recording", for: .normal)
+            changeButtonToStart()
+            if(audioPlayer != nil && audioPlayer.isPlaying){
+                audioPlayer.stop()
+                audioPlayer = nil
+                message.text = "Audio playing is stopped, tap agagin to restart"
+            }
+            else{
+                audioRecorder.stop()
+                audioRecorder = nil
+                UserDefaults.standard.set(numberOfRecording, forKey: "recodingNumber")
+                recordingTableView.reloadData()
+                message.text = "Finished recording"
+            }
         }
     }
     
@@ -81,23 +102,41 @@ class AudioViewController: UIViewController, AVAudioRecorderDelegate, UITableVie
         present(alert, animated: true, completion: nil)
     }
     
+    // MARK: - button view
+    
+    func changeButtonToStop(){
+        StartStopButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.stop, target: self, action: #selector(recordORstop(_:)))
+        toolBar.items![1] = StartStopButton
+    }
+    
+    func changeButtonToStart(){
+        StartStopButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.play, target: self, action: #selector(recordORstop(_:)))
+        toolBar.items![1] = StartStopButton
+    }
+    
     // MARK: - table view
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return numberOfRecording
     }
     
+    // return cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "recordingCell", for: indexPath)
         cell.textLabel?.text = String(indexPath.row + 1) // name
         return cell
     }
     
+    // when cell is selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let path = getDirectory().appendingPathComponent("\(indexPath.row+1).m4a")
         do{
             audioPlayer = try AVAudioPlayer(contentsOf: path)
             audioPlayer.play()
+            changeButtonToStop()
+            audioPlayer.numberOfLoops = 0
+            audioPlayer.delegate = self
+            message.text = "Audio is playing..."
         }
         catch{
             
@@ -114,4 +153,17 @@ class AudioViewController: UIViewController, AVAudioRecorderDelegate, UITableVie
     }
     */
 
+}
+
+extension AudioViewController: AVAudioPlayerDelegate {
+    // stop audioPlayer when audio is finished
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        if flag {
+            print("Audio player finished playing")
+            self.audioPlayer?.stop()
+            self.audioPlayer = nil
+            changeButtonToStart()
+            message.text = "Finished audio playing"
+        }
+    }
 }
