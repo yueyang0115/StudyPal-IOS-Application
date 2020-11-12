d0cf26097e95b3dc498afd4827e6c65578abaeb7
yueyang <yueyang0115@outlook.com>
2020-11-12 12:22:38 -0500

    add choose and control timer

---
+++ ECE564project/CollectionRelated/TimeHelper.swift
@@ -0,0 +1,14 @@
+//
+//  TimeHelper.swift
+//  ECE564project
+//
+//  Created by 杨越 on 11/12/20.
+//  Copyright © 2020 杨越. All rights reserved.
+//
+
+import Foundation
+
+enum TimerMode{
+    case running
+    case initial
+}
--- ECE564project/ViewController/CounterViewController.swift
+++ ECE564project/ViewController/CounterViewController.swift
@@ -8,25 +8,81 @@
 
 import UIKit
 
-class CounterViewController: UIViewController {
+class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    @IBOutlet weak var timeLabel: UILabel!
     var circlePath: UIBezierPath!
-    
     let shapeLayer = CAShapeLayer()
     var pulsatingLayer = CAShapeLayer()
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var startImage: UIImageView!
+    var selectPickerIndex = 0
+    
+    var timer = Timer()
+    var time:Int = 0
+    var timerMode: TimerMode = .initial
+    let availableMinutes = Array(1...60)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setCirclePath()
-        //createPulsatingLayer()
         createBackgroundLayer()
         createShapeLayer()
-        //view.addSubview(timeLabel)
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func controlTimer(_ sender: Any) {
+        if(timerMode == .initial){
+            if(time != 0){
+                startTimer()
+                timerMode = .running
+                startButton.setTitle("pause", for: .normal)
+                pickerView.isHidden = true
+            }
+        }
+        else{
+            timer.invalidate()
+            timerMode = .initial
+            startButton.setTitle("start", for: .normal)
+        }
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        timer.invalidate()
+        timerMode = .initial
+        time = 0
+        timeLabel.text = "00:00"
+        startButton.setTitle("start", for: .normal)
+        pickerView.isHidden = false
+    }
+    
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if(time == 0){
+            timer.invalidate()
+            time = 0
+            timerMode = .initial
+            timeLabel.text = "0"
+            startButton.setTitle("start", for: .normal)
+        }
+        time -= 1
+        timeLabel.text = formatTime(time: time)
+    }
+    
+    func formatTime(time: Int)->String{
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+        return String(format:"%02i:%02i", minutes, seconds)
+    }
     
     func setCirclePath(){
         circlePath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
@@ -44,32 +100,7 @@ class CounterViewController: UIViewController {
         
         view.layer.addSublayer(backgroundLayer)
     }
-    
-    func createPulsatingLayer(){
-        //pulsatingLayer = CAShapeLayer()
-        pulsatingLayer.path = circlePath.cgPath
-        pulsatingLayer.strokeColor = UIColor.gray.cgColor
-        pulsatingLayer.lineWidth = 10
-        pulsatingLayer.fillColor = UIColor.pulsatingFillColor.cgColor
-        pulsatingLayer.lineCap = .round
-        pulsatingLayer.position = view.center
         
-        view.layer.addSublayer(pulsatingLayer)
-        animatePulsatingLayer()
-    }
-    func animatePulsatingLayer(){
-        print("try scale")
-        let animation = CABasicAnimation(keyPath: "transform.scale")
-        
-        animation.toValue = 1.3
-        animation.duration = 0.8
-        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
-        animation.autoreverses = true
-        animation.repeatCount = Float.infinity
-
-        pulsatingLayer.add(animation, forKey: "pulsing")
-    }
-    
     func createShapeLayer() {
         shapeLayer.path = circlePath.cgPath
         shapeLayer.strokeColor = UIColor.outlineStrokeColor.cgColor
@@ -120,4 +151,28 @@ class CounterViewController: UIViewController {
     }
     */
 
+     // MARK: - set up pickerView
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//        let res: String = "\(availableMinutes[row]) min"
+//        time = availableMinutes[row] * 60
+//        return res
+//    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let res: String = "\(availableMinutes[row]) min"
+        time = availableMinutes[row] * 60
+        return NSAttributedString(string: res, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return availableMinutes.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        timeLabel.text = formatTime(time: availableMinutes[row] * 60)
+    }
 }
