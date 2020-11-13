e7bf3251354bea46286565a4f8c0de8ec404ba44
yueyang <yueyang0115@outlook.com>
2020-11-12 19:47:18 -0500

    refactor code

--- ECE564project/ViewController/CounterViewController.swift
+++ ECE564project/ViewController/CounterViewController.swift
@@ -40,34 +40,23 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         timeLabel.text = ""
         setCirclePath()
         createBackProgressLayer()
-        //createForeProgressLayer()
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         self.foreProgressLayer = passforeProgressLayer
         self.animation = passanimation
         
-        
-        
+        // begin a new start
         if(passTime == 0){
             timeLabel.text = formatTime(time: 0)
             startButton.setTitle("Start", for: .normal)
             startButton.setTitleColor(UIColor.outlineStrokeColor, for: .normal)
         }
+        // continue with last timer and animation
         else{
             time = passTime
             timeLabel.text = formatTime(time: time)
-            
-            
-            //foreProgressLayer.add(animation, forKey: "strokeEnd")
-            
-            //createForeProgressLayer()
-            resumeAnimation()
-            startTimer()
-            timerMode = .running
-            startButton.setTitle("Pause", for: .normal)
-            startButton.setTitleColor(UIColor.orange, for: .normal)
+            doPause() // pause previous timer and animation
             pickerView.isHidden = true
         }
     }
@@ -75,27 +64,34 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     // MARK: - set up timer
     @IBAction func controlTimer(_ sender: Any) {
         if(timerMode == .initial){
-            if(time != 0){
+            if(time != 0){ // start new timer and progress animation
                 createForeProgressLayer()
-                startResumeAnimation()
-                startTimer()
-                timerMode = .running
-                startButton.setTitle("Pause", for: .normal)
-                startButton.setTitleColor(UIColor.orange, for: .normal)
-                pickerView.isHidden = true
+                doStart()
             }
         }
-        else{
-            pauseAnimation()
-            timer.invalidate()
-            timerMode = .initial
-            startButton.setTitle("Start", for: .normal)
-            startButton.setTitleColor(UIColor.outlineStrokeColor, for: .normal)
+        else{ // pause timer and progress animation
+            doPause()
         }
     }
     
-    @IBAction func cancel(_ sender: Any) {
-        stopAnimation()
+    func doStart(){
+        startResumeAnimation()
+        startTimer()
+        timerMode = .running
+        startButton.setTitle("Pause", for: .normal)
+        startButton.setTitleColor(UIColor.orange, for: .normal)
+        pickerView.isHidden = true
+    }
+    
+    func doPause(){
+        pauseAnimation()
+        timer.invalidate()
+        timerMode = .initial
+        startButton.setTitle("Start", for: .normal)
+        startButton.setTitleColor(UIColor.outlineStrokeColor, for: .normal)
+    }
+    
+    func doStop(){
         timer.invalidate()
         timerMode = .initial
         time = 0
@@ -105,27 +101,24 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         pickerView.isHidden = false
     }
     
+    // cancel current timer and progress animation
+    @IBAction func cancel(_ sender: Any) {
+        stopAnimation()
+        doStop()
+    }
+    
     func startTimer(){
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer(){
         if(time <= 0){
-            timer.invalidate()
-            time = 0
-            timerMode = .initial
-            timeLabel.text = formatTime(time: time)
-            startButton.setTitle("start", for: .normal)
-            startButton.setTitleColor(UIColor.outlineStrokeColor, for: .normal)
-            pickerView.isHidden = false
+            doStop()
         }
         else{
             time -= 1
             timeLabel.text = formatTime(time: time)
         }
-//        if(!isAniationStarted && timerMode == .running){
-//            startAnimationTime(time: time)
-//        }
     }
     
     func formatTime(time: Int)->String{
@@ -148,7 +141,6 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         backProgressLayer.fillColor = UIColor.clear.cgColor
         backProgressLayer.lineCap = .round
         backProgressLayer.position = view.center
-        
         view.layer.addSublayer(backProgressLayer)
     }
         
@@ -161,7 +153,6 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         foreProgressLayer.lineCap = .round
         foreProgressLayer.position = view.center
         foreProgressLayer.transform = CATransform3DMakeRotation(-CGFloat.pi/2, 0, 0, 1)
- 
         view.layer.addSublayer(foreProgressLayer)
     }
     
@@ -189,21 +180,6 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         isAniationStarted = true
     }
     
-//    func startAnimationTime(time: Int) {
-//        resetAnimation()
-//        foreProgressLayer.strokeEnd = 0.0
-//        animation.keyPath = "strokeEnd"
-//        animation.fromValue = 0
-//        animation.toValue = 1
-//        animation.duration = CFTimeInterval(time)
-//        animation.delegate = self
-//        animation.isRemovedOnCompletion = false
-//        animation.isAdditive = true
-//        animation.fillMode = CAMediaTimingFillMode.forwards
-//        foreProgressLayer.add(animation, forKey: "strokeEnd")
-//        isAniationStarted = true
-//    }
-    
     func pauseAnimation(){
         let pausedTime = foreProgressLayer.convertTime(CACurrentMediaTime(), from: nil)
         foreProgressLayer.speed = 0.0
@@ -228,20 +204,14 @@ class CounterViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     }
     
     func stopAnimation(){
-        foreProgressLayer.speed = 1.0
-        foreProgressLayer.timeOffset = 0.0
-        foreProgressLayer.beginTime = 0.0
-        foreProgressLayer.strokeEnd = 0.0
+        resetAnimation()
         foreProgressLayer.removeAllAnimations()
-        isAniationStarted = false
     }
     
     internal func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
         if(!isViewChange){
+            doStop()
             stopAnimation()
-        }else if(time != 0 && isAniationStarted){
-            pauseAnimation()
-            resumeAnimation()
         }
     }
 
